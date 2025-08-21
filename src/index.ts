5932b9d17d07447b9bede73687e839d814eb17ba
Cold <103373668+cColds@users.noreply.github.com>
2025-08-21 18:54:55 -0500

    Fix build issues and move views out of src

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { shareRouter } from "./routes/share";
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
